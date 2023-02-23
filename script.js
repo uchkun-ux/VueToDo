b7e5a5286a875763d840787324a309039162c74b
Uchkun <85468493+uchkun-ux@users.noreply.github.com>
2023-02-23 22:55:16 +0500

    create project

---
+++ script.js
@@ -0,0 +1,34 @@
+const object = {
+    data(){
+        return {
+        list:[],
+        title:"",
+        describe:""
+        }
+    },
+    methods:{
+        addTask(){
+            if (this.title=="" || this.describe == ""){
+
+            }
+            this.list.push({
+                title: this.title, 
+                describe:this.describe,
+                time: new Date().toLocaleString(),
+                done: true,
+                class: ""
+            })
+            this.title = ""
+            this.describe = ""
+        },
+        change(i){
+            this.list.forEach((el, index) => {
+                if (i==index){
+                el.done = !el.done
+                el.class = el.class == "" ? "bg" : ""
+            }
+            });
+        }
+    }
+}
+Vue.createApp(object).mount('.counter') 
